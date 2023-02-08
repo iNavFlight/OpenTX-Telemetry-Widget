44716a8a983705f3729d6fddfbe8e13ff1bf1236
Jonathan Hudson <jh+github@daria.co.uk>
2023-02-08 14:42:25 +0000

    improved ELRSv3 detection from @CapnBry

--- src/SCRIPTS/TELEMETRY/iNav/elrs.lua
+++ src/SCRIPTS/TELEMETRY/iNav/elrs.lua
@@ -1,24 +1,10 @@
 -- Adapted from ELRSv2.lua in ExpressLRS
 
-local deviceId = 0xEE
-
-local function byteToStr(b)
-   -- Translate b into a string from symbolChars if available, else use string.char
-   return symbolChars and symbolChars[b] or string.char(b)
-end
-
-local function fieldGetString(data, offset, last)
-   if last then
-      return fieldStrFF(data, offset, last)
-   end
-
-   local result = ""
-   while data[offset] ~= 0 do
-      result = result .. byteToStr(data[offset])
-      offset = offset + 1
-   end
-
-   return result, offset + 1
+local function fieldStrFF(data, offset)
+  while data[offset] ~= 0 do
+    offset = offset + 1
+  end
+  return offset + 1
 end
 
 local function fieldGetValue(data, offset, size)
@@ -29,49 +15,19 @@ local function fieldGetValue(data, offset, size)
    return result
 end
 
---[[
-local function debug_devinfo(data)
-   local ut = getRtcTime()
-   local s = string.format("/LOGS/E_%d.txt", ut)
-   local fh = io.open(s,"wb")
-   dbg_serialize(fh, data)
-   io.close(fh)
-end
-
-local function dbg_serialize (fh, o)
-   if type(o) == "number" then
-      io.write(fh, o)
-   elseif type(o) == "string" then
-      io.write(fh, string.format("%q", o))
-   elseif type(o) == "table" then
-      io.write(fh, "{\n")
-      for k,v in pairs(o) do
-	 io.write(fh, "  [")
-	 dbg_serialize(fh, k)
-	 io.write(fh, "] = ")
-	 dbg_serialize(fh, v)
-	 io.write(fh, ",\n")
-      end
-      io.write(fh, "}\n")
-   else
-      print("cannot serialize a " .. type(o))
-   end
-end
---]]
-
---  See https://github.com/iNavFlight/OpenTX-Telemetry-Widget/issues/70#issuecomment-1383190840
+--  See https://github.com/ExpressLRS/ExpressLRS/wiki/CRSF-Protocol#device-info--device-ping-response-0x29
 --  For annotation
-
 local function parseDeviceInfoMessage(data)
-   local offset
-   local id = data[2]
-   local devicesName
    local majId = -1
-   devicesName, offset = fieldGetString(data, 3)
-   if deviceId == id then
+   local deviceId = data[2]
+   local offset = fieldStrFF(data, 3)
+   if deviceId == 0xEE then -- TX device
       -- debug_devinfo(data)
-      if ((fieldGetValue(data,offset,4) == 0x454C5253) and (deviceId == 0xEE)) then
-	 majId = data[offset+9]
+      if fieldGetValue(data,offset,4) == 0x454C5253 then -- 'ELRS'
+        majId = data[offset+9]
+        if majId == 0 then
+          majId = 2 -- v2 did not report version, v1 didn't support this protocol
+        end
       end
    end
    return majId
